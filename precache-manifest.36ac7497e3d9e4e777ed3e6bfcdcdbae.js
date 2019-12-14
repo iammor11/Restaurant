48e99a33326f9a147e226236b917f9713775208a
unknown <osamarizwan444@gmail.com>
2019-12-15 01:39:29 +0500

    Updates

---
+++ precache-manifest.36ac7497e3d9e4e777ed3e6bfcdcdbae.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0829397f79458295ef475ff3793b8dc8",
+    "url": "/restaurant/index.html"
+  },
+  {
+    "revision": "fd4f4cdc58d5e60f18b8",
+    "url": "/restaurant/static/css/main.e7a72f03.chunk.css"
+  },
+  {
+    "revision": "a1fa3ae757be37405012",
+    "url": "/restaurant/static/js/2.a42d3222.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/restaurant/static/js/2.a42d3222.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fd4f4cdc58d5e60f18b8",
+    "url": "/restaurant/static/js/main.c84af1d7.chunk.js"
+  },
+  {
+    "revision": "0dec8ed59e3358a180e2",
+    "url": "/restaurant/static/js/runtime-main.6a6f159a.js"
+  },
+  {
+    "revision": "6c19c29d7d1f9e9c12b08218ba2a80fd",
+    "url": "/restaurant/static/media/1.6c19c29d.jpg"
+  },
+  {
+    "revision": "6dfd6a8cc00186ee9085735a0d823b7a",
+    "url": "/restaurant/static/media/2.6dfd6a8c.jpg"
+  },
+  {
+    "revision": "c8dc247a6bd877a826250cd434fcf664",
+    "url": "/restaurant/static/media/3.c8dc247a.jpg"
+  },
+  {
+    "revision": "4b50bccaa00ac6bc3c21ec2d312b6ac4",
+    "url": "/restaurant/static/media/cd.4b50bcca.jpg"
+  },
+  {
+    "revision": "37615d7a5e0943376557522160fc315e",
+    "url": "/restaurant/static/media/dinner.37615d7a.jpg"
+  },
+  {
+    "revision": "b66a9de4432ae443bd70be17dc410805",
+    "url": "/restaurant/static/media/dinner1.b66a9de4.jpg"
+  },
+  {
+    "revision": "084571d5a7916ad68089361665622562",
+    "url": "/restaurant/static/media/drink.084571d5.jpg"
+  },
+  {
+    "revision": "9861d981dba94278c78ca70b9f8b406f",
+    "url": "/restaurant/static/media/fastfood.9861d981.jpg"
+  },
+  {
+    "revision": "6a8e667731d188aba425d3e98efa5bea",
+    "url": "/restaurant/static/media/fb.6a8e6677.png"
+  },
+  {
+    "revision": "f6c0eda5fa88ef6f7ddfc7a1be731ace",
+    "url": "/restaurant/static/media/ff.f6c0eda5.jpg"
+  },
+  {
+    "revision": "d59c3272f82bbc6ebb821b5784004843",
+    "url": "/restaurant/static/media/gallery1.d59c3272.jpg"
+  },
+  {
+    "revision": "298e9669e28112a54115be0933a1fc1d",
+    "url": "/restaurant/static/media/ice2.298e9669.png"
+  },
+  {
+    "revision": "c35ec174ab69cbe7d69fda4804fac02a",
+    "url": "/restaurant/static/media/icecreame.c35ec174.jpg"
+  },
+  {
+    "revision": "778c3951b6f5dac1f13b6d13e00938a8",
+    "url": "/restaurant/static/media/image_6.778c3951.jpg"
+  },
+  {
+    "revision": "67e94bbce8a753ee6ff7c2c131379c07",
+    "url": "/restaurant/static/media/ins1.67e94bbc.jpg"
+  },
+  {
+    "revision": "a570faaffbd41b83add4f0e06a27babd",
+    "url": "/restaurant/static/media/logo.a570faaf.png"
+  },
+  {
+    "revision": "caa2d4ce1efd8b3c5513df7ab699c9cf",
+    "url": "/restaurant/static/media/lunch2.caa2d4ce.jpg"
+  },
+  {
+    "revision": "36e1cb435d9e5c1ccaf5ec7de2d8ab17",
+    "url": "/restaurant/static/media/myteam.36e1cb43.jpg"
+  },
+  {
+    "revision": "cb4a1524b3c26737189a1dc4e69a5ec9",
+    "url": "/restaurant/static/media/party1.cb4a1524.svg"
+  },
+  {
+    "revision": "d9813d1d215caa623e0209ab3f804850",
+    "url": "/restaurant/static/media/restaurant1.d9813d1d.svg"
+  },
+  {
+    "revision": "6dc927288da69020f5360a25cc13e1bd",
+    "url": "/restaurant/static/media/restaurant4.6dc92728.svg"
+  }
+]);
